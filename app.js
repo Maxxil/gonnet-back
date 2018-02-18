cc1e301fe5b4bf8f7c5ed6d365fa8c105301b26c
unknown <massil.kadi@ynov.com>
2018-02-18 15:39:53 +0100

    Ajout de api.gonnet.ovh a cors

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ var domains = [
     'https://www.anthony-gonnet.com',
     'http://localhost:4200',
     'http://localhost:63342',
-    'http://www.api.gonnet.ovh/',
-    'https://www.api.gonnet.ovh/'
+    'http://api.gonnet.ovh/',
+    'https://api.gonnet.ovh/'
 ];
 
 var corsOptions = {
