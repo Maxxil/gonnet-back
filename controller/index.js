207a2afb0e39e0bc675d6dbae64d12c45a02b98f
unknown <massil.kadi@ynov.com>
2018-02-19 19:45:15 +0100

    Passage de https a http

--- controller/index.js
+++ controller/index.js
@@ -11,6 +11,9 @@ router.use('/signin' , require('./signinController'));
 router.use('/mail' , require('./mailController'));
 router.use('/project', require('./projectController'));
 router.use('/projectByType' , require('./projectByTypeController'));
+router.use('/favincon.ico' , function (req, res) {
+    res.end('No icon');
+});
 router.all('*', function (req, res) {
     res.end('Home page');
 });
